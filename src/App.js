e67c377b0634c9f2811b2335f50fff6b31803f7f
Caique Costa <ccostafrias@gmail.com>
2023-11-08 19:18:52 -0300

    place hex

--- src/App.js
+++ src/App.js
@@ -2,12 +2,42 @@ import React, { useState, useEffect } from "react"
 
 
 export default function App() {
+    const lines = 5
+    const hexCount = 5
+    const end = 0
+
+    const hexLines = Array.from(Array(lines))
+    const hexElement = (hc) => {
+        return (
+            Array.from(Array(hc)).map(hex => (
+                <div className="hex"></div>
+            ))
+        )
+    }
+
+    const hexElements = hexLines.map((hl, i) => {
+        return (
+            <div className="hex-line">
+                {hexElement(hexCount + (i % 2))}
+            </div>
+        )
+
+    })
+
+    const hexEnd = Array.from(Array(end)).map((he, i) => {
+        return (
+            <div className="hex-line">
+                {hexElement(hexCount - (end - i))}
+            </div>
+        )
+    })
 
     return (
-        <>
-            <main>
-            </main>
-        </>
+        <main>
+            {hexEnd}
+            {hexElements}
+            {[...hexEnd].reverse()}
+        </main>
        
     )
 }
